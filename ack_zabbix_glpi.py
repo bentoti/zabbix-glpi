38d74a50a21834188f314eebd299db37667fc9e8
Vitor Mazuco <vitor.mazuco@gmail.com>
2020-02-12 10:30:22 -0300

    Update ack_zabbix_glpi.py

--- ack_zabbix_glpi.py
+++ ack_zabbix_glpi.py
@@ -14,4 +14,4 @@ conexao.login(username, password)
 
 reconhecer_evento = conexao.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no GLPI."})
 
-# Esse código foi publicado originalmete pelo @Janssen dos Reis Lima
+# Esse código foi publicado originalmete pelo https://github.com/janssenlima/zabbix-glpi
